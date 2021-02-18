7cddf0bb35da4e905465ead74baec0b20309d944
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2021-02-18 20:12:37 -0300

    Att

--- Resources/main.js
+++ Resources/main.js
@@ -13,6 +13,8 @@ let c1 = moves[6]
 let c2 = moves[7]
 let c3 = moves[8]
 
+let vencedor = null
+
 
 function play(square, place) {
 
@@ -40,24 +42,98 @@ function play(square, place) {
         document.getElementById("error").style.display = "block"
     }
 
-    winCondition()
+    // Win Conditions
 
-}
+    if ((a1[0] == a2[0] && a1[0] == a3[0] && a1[0] != '') || (a1[0] == b1[0] && a1[0] == c1[0] && a1[0] != '')
+        || a1[0] == b2[0] && a1[0] == c3[0] && a1[0] != '') {
 
-function winCondition() {
+        vencedor = a1[0]
 
-    if (a1[0] == a2[0] && a1[0] == a3[0] && a1[0] != '') {
-        console.log('YOU WIN')
+    } else if ((b2[0] == b1[0] && b2[0] == b3[0] && b2[0] != '') || (b2[0] == a2[0] && b2[0] == c2[0] && b2[0] != '')
+        || b2[0] == a3[0] && b2[0] == c1[0] && b2[0] != '') {
+
+        vencedor = b2[0]
+
+    } else if ((c3[0] == c2[0] && c3[0] == c1[0] && c3[0] != '') || (c3[0] == b3[0] && c3[0] == a3[0] && c3[0] != '')) {
+
+        vencedor = c3[0]
+
     }
+
+    if (vencedor == 'x') {
+
+        document.querySelector("#win-container").style.display = "flex"
+        document.querySelector("#win-container h1").innerHTML = "Player 1 Wins!"
+        document.querySelector("#player-turn h1").style.display = "none"
+
+    } if (vencedor == 'o') {
+
+        document.querySelector("#win-container").style.display = "flex"
+        document.querySelector("#win-container h1").innerHTML = "Player 2 Wins!"
+        document.querySelector("#player-turn h1").style.display = "none"
+
+    }
+
+}
+
+function start() {
+
+    document.getElementById('menu-container').style.display = 'none'
+    document.getElementById('win-container').style.display = 'none'
+    document.getElementById('container').style.display = 'flex'
+
+}
+
+function restart() {
+
+    document.getElementById('win-container').style.display = 'none'
+
+    for (i = 0; i < 9; i++) {
+        moves.shift()
+        moves.push([])
+    }
+
+    a1 = moves[0]
+    a2 = moves[1]
+    a3 = moves[2]
+    b1 = moves[3]
+    b2 = moves[4]
+    b3 = moves[5]
+    c1 = moves[6]
+    c2 = moves[7]
+    c3 = moves[8]
+
+    player = true
+    vencedor = null
+
+    for (i = 1; i < 4; i++) {
+
+        document.getElementById('a' + i).classList.remove("Oplayed")
+        document.getElementById('b' + i).classList.remove("Oplayed")
+        document.getElementById('c' + i).classList.remove("Oplayed")
+        document.getElementById('a' + i).classList.remove("Xplayed")
+        document.getElementById('b' + i).classList.remove("Xplayed")
+        document.getElementById('c' + i).classList.remove("Xplayed")
+
+    }
+
+    document.querySelector("#player-turn h1").style.display = "block"
+    document.querySelector("#player-turn h1").innerHTML = "Vez do jogador X"
+
+}
+
+function backMenu() {
+
+    window.location.reload()
+
 }
 
 
-// CONDIÇOES PARA WIN
+function showInstructions() {
+    document.getElementById('instructions-board').style.display = 'flex'
+}
+function exitInstructions() {
+    document.getElementById('instructions-board').style.display = 'none'
+}
 
-
-/* OKKK
-JOGADOR1 CLICA
-BACKGROUND DE x NO QUADRADO
-REGISTRA QUAL QUADRADO O x CLICOU NO ARRAY
-MUDA PRO PLAYER 2
-*/
+//jogador 1 = true
